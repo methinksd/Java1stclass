a01627fb20c5a1121a4a346f72f82d6aac331cbc
methinksd <104886364+methinksd@users.noreply.github.com>
2023-09-13 14:09:00 +0300

    Add files via upload

---
+++ Marksheet.java
@@ -0,0 +1,46 @@
+package net.codejava;
+
+public class Marksheet {
+
+	public static void main(String[] args) {
+		int score1 =70;
+		int score2 =68;
+		int score3 =74;
+		int score4 =65;
+		int Total =277;
+		float Average =69.25f;
+		
+		
+	  System.out.println("\t\t\tDepartment of Computer Science");
+	  System.out.println("\t\t\t\tEnd Semester Results");
+	  System.out.println("---------------------------------------------------------------");
+	  System.out.println("|Name:-\t\t\t|Roll_No:-\t\t\tSemester:-");
+	  System.out.println("_______________________________________________________________");
+	  System.out.println("|Unit Code:-\t\t|Unit Name:-\t\t\tScore:-");
+	  System.out.println("-----------------------------------------------------------------");
+	  System.out.println("|CCS2101\t\t|Database\t\t\t|"+score1);
+	  System.out.println("|CCS2101\t\t|System\t\t\t\t|"+score2);
+	  System.out.println("|CCS2101\t\t|Algorithm\t\t\t|"+score3);
+	  System.out.println("|CCS2101\t\t|Statistics\t\t\t|"+score4);
+	  System.out.println("-----------------------------------------------------------------");
+	  System.out.println("|Total \t\t\t\t\t\t\t|"+Total);
+	  System.out.println("------------------------------------------------------------------");
+	  System.out.println("|Average \t\t\t\t\t\t|"+Average);
+	  System.out.println("------------------------------------------------------------------");
+	  System.out.println("|Grade \t\t\t\t\t\t\t|A");
+	  System.out.println("-------------------------------------------------------------------");
+	  System.out.println("\t\t\t\t\tRecommendation|");
+	  System.out.println("--------------------------------------------------------------------"); 
+	  System.out.println("Grading Criteria\t\tOverall Grade(A-D)\t\t Recommend Pass");
+	  System.out.println("70-100 \t\t A");
+	  System.out.println("60-69 \t\t B");
+	  System.out.println("50-59 \t\t C");
+	  System.out.println("40-49 \t\t D");
+	  System.out.println("Below 40\t F");
+	  System.out.println("-------------------------------------------------------------------------");
+	  
+	  
+	  
+	}
+
+}
